e805d4cbb028ade3ef86100782cfead26245ebd8
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2025-02-03 13:39:23 -0500

    updates

--- COI-Proxmox.ps1
+++ COI-Proxmox.ps1
@@ -13,14 +13,6 @@ $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 
-$script:Vars = @{
-    Credentials = $null
-    Headers = @{
-        Authorization = $null
-        CSRFPreventionToken = $null
-    }
-}
-
 class ApiException : System.Exception {
     [int]$StatusCode
     [string]$response
@@ -39,38 +31,43 @@ function Invoke-PVEAPI {
     Param (
         [Parameter(Mandatory)][string]$Route,
         [Parameter(Mandatory)][hashtable]$Params,
+        [switch]$Silent,
         [string]$ErrorBehavior = "Continue"
     )
 
     $err = $false
+    # Insert the URI and ContentType header into the Params hashtable
     $Params["Uri"] = "https://172.28.116.111:8006/api2/json/$route"
     $Params["ContentType"] = "application/x-www-form-urlencoded"
 
     try {
+        # Send an HTTP request to the Proxmox API
         $response = Invoke-WebRequest @Params
         if (($response.StatusCode -ne 200) -and ($response.StatusCode -ne 201)) {
             throw [ApiException]::new("Request failed", $response.StatusCode, $response.StatusDescription)
         }
         $to_return = $response.Content | ConvertFrom-Json
     }
+    # The goal is to return something even if the request fails so the calling function can decide what to do on failure
     catch [System.Net.WebException] {
         $err = $true
 
-        Write-Warning "$($_.Exception.Message) to route $($route) with method $($Params.Method)"
+        if (-not $Silent) {Write-Warning "$($_.Exception.Message) to route $($route) with method $($Params.Method)"}
 
         if ($_.Exception.Response.StatusCode.value__) {
-            $to_return = @{Error = $true; StatusCode = $_.Exception.Response.StatusCode.value__}
+            $to_return = @{Error = $true; StatusCode = $_.Exception.Response.StatusCode.value__; Response = $_.Exception.Response.StatusDescription}
         }
         else {
             $to_return = @{Error = $true}
         }
     }
     catch [ApiException] {
-        Write-Warning "$($_.Exception) to route $($route) with method $($Params.Method)"
+        if (-not $Silent) {Write-Warning "$($_.Exception.Message) to route $($route) with method $($Params.Method)"}
         $err = $true
         $to_return = @{StatusCode = $_.Exception.StatusCode; Error = $true}
     }
 
+    # The ErrorBehavior flag overrides the default behavior of returning something on failure and just throws an error. This is useful for when the program shouldn't continue if later processing requires certain data/objects to exist in Proxmox
     if ((-not $err) -or ($ErrorBehavior -eq "Continue")) {
         return $to_return
     }
@@ -80,26 +77,29 @@ function Invoke-PVEAPI {
 }
 
 function Get-AccessTicket {
-    Param (
-        [Parameter(Mandatory)][pscredential]$Credentials
-    )
-
-    $params = @{
-        Method = "POST"
-        Body = @{
-            username = $Credentials.UserName
-            password = $Credentials.GetNetworkCredential().Password
-            realm = "NKU"
+    # To prevent re-authenticating within the same PowerShell session, the access ticket and CSRF token are stored in (ephemeral) environment variables
+    if (-not ($env:ACCESS_TICKET -and $env:CSRF_TOKEN)) {
+        $Credentials = (Get-Credential -Credential $env:USERNAME)
+        $params = @{
+            Method = "POST"
+            Body = @{
+                username = $Credentials.UserName
+                password = $Credentials.GetNetworkCredential().Password
+                realm = "NKU"
+            }
         }
+
+        $ticket = Invoke-PVEAPI -Route "access/ticket" -Params $params -ErrorBehavior "Stop"
+        $env:ACCESS_TICKET = "PVEAuthCookie=$($ticket.data.ticket)"
+        $env:CSRF_TOKEN = $ticket.data.CSRFPreventionToken
+
+        Write-Host "Session API keys successfully obtained" -ForegroundColor Green
     }
 
-    $ticket = Invoke-PVEAPI -Route "access/ticket" -Params $params -ErrorBehavior "Stop"
-    
-    $Vars.Headers.Authorization = "PVEAuthCookie=$($ticket.data.ticket)"
-    $Vars.Headers.CSRFPreventionToken = $ticket.data.CSRFPreventionToken
-
-    if ($Vars.Headers.Authorization -and $Vars.Headers.CSRFPreventionToken) {
-        Write-Host "Session API key successfully obtained" -ForegroundColor Green
+    # Authorization headers don't use the environment variables directly, rather this function just returns them as a hashtable. This is because environment variables have to be strings and I don't want to construct the hashtable every time I need to use them.
+    return @{
+        Authorization = $env:ACCESS_TICKET
+        CSRFPreventionToken = $env:CSRF_TOKEN
     }
 }
 
@@ -108,22 +108,24 @@ function Create-Pool {
         [Parameter(Mandatory)][string]$id
     )
 
-    $pools = Invoke-PVEAPI -Route "pools" -Params @{Headers = $Vars.Headers; Method="GET"} -ErrorBehavior "Stop"
+    $pools = Invoke-PVEAPI -Route "pools" -Params @{Headers = (Get-AccessTicket); Method="GET"} -ErrorBehavior "Stop"
 
+    # Check if the pool exists before creating it
     if ($id -in ($pools.data | Select -ExpandProperty poolid)) {
         Write-Warning "Pool $id already exists. Skipping creation..."
     } 
     else {
-        $response = Invoke-PVEAPI -Route "pools" -Params @{Headers = $Vars.Headers; Method="POST"; Body=@{poolid=$id}}
+        # Try to create the pool, retry one time in case it fails for a reason other than invalid authorization
+        $response = Invoke-PVEAPI -Route "pools" -Params @{Headers = (Get-AccessTicket); Method="POST"; Body=@{poolid=$id}}
         if ($response.Error -and ($response.StatusCode -ne 401)) {
             Write-Host "Retrying once..." -ForegroundColor White
-            Invoke-PVEAPI -Route "pools" -Params @{Headers = $Vars.Headers; Method="POST"; Body=@{poolid=$id}} -ErrorBehavior Stop
+            Invoke-PVEAPI -Route "pools" -Params @{Headers = (Get-AccessTicket); Method="POST"; Body=@{poolid=$id}} -ErrorBehavior Stop
         }
         elseif ($response.StatusCode -eq 401) {
             throw "Unrecoverable HTTP status 401 returned; make sure you have the correct privileges on Proxmox."
         }
         else {
-            Write-Host "Created pool with ID $($id -replace ' ','')" -ForegroundColor Green
+            Write-Host "Created pool with ID $id" -ForegroundColor Green
         }
     }
 }
@@ -132,6 +134,7 @@ function Get-NextNode {
         [Parameter(Mandatory)][int]$LastIndex
     )
 
+    # Return the next node to use in a circular fashion. This is just round robin load balancing. 
     $nodes = @("COIVMHOST1", "COIVMHOST2", "COIVMHOST3", "COIVMHOST4")
     $next_index = ($LastIndex + 1) % $nodes.length
 
@@ -174,11 +177,39 @@ function Get-Templates {
         [Parameter(Mandatory)][string]$Class
     )
 
+    # Given a class code, return all the templates this class uses.
     $Class = $Class.Split("-")[0] -replace ' ',''
-    $data = Invoke-PVEAPI -Route "pools/Templates" -Params @{Headers = $Vars.Headers; Method="GET"} -ErrorBehavior "Stop"
+    $data = Invoke-PVEAPI -Route "pools/Templates" -Params @{Headers = (Get-AccessTicket); Method="GET"} -ErrorBehavior "Stop"
     $template_list = @($data.data.members | ? {$_.name -match $Class} | Select name,vmid)
 
     return $template_list
+}
+
+function Set-ACL {
+    Param (
+        [Parameter(Mandatory)][string]$Professor,
+        [Parameter(Mandatory)][string]$User,
+        [Parameter(Mandatory)][string]$ID
+    )
+
+    $acl_body = @{
+        path = "/vms/$id"
+        users = "$user@NKU,$professor@NKU"
+        roles = "Faculty-Students"
+    }
+
+    # Update the access control list to include student/professor permissions for a VM.
+    $response = Invoke-PVEAPI -Route "access/acl" -Params @{Headers = (Get-AccessTicket); Method = "PUT"; Body = $acl_body} -Silent
+
+    if ($response.Response -match "ACL update failed: user '($User@NKU|$Professor@NKU)' does not exist") {
+        Write-Warning "$($response.Response). The user likely has not been synced to the NKU realm."
+    }
+    elseif (($response.StatusCode) -and ($response.StatusCode -ne 200)) {
+        Write-Warning "Failed to set ACL for $($User): $($response.StatusCode)"
+    }
+    else {
+        Write-Host "ACL set for $ID; $User, $Professor"
+    }
 }
 
 function Clone-ClassVMs {
@@ -191,7 +222,7 @@ function Clone-ClassVMs {
     $student_list, $professor = $class_roster[0], $class_roster[1]
     $users = @($student_list) + $professor
 
-    $current_vms = Invoke-PVEAPI -Route "nodes/COIVMHOST1/qemu" -Params @{Headers = $Vars.Headers; Method="GET"}
+    $current_vms = Invoke-PVEAPI -Route "nodes/COIVMHOST1/qemu" -Params @{Headers = (Get-AccessTicket); Method="GET"}
     $vm_id = ($current_vms.data | Select -ExpandProperty vmid | Sort)[-1] + 1
 
     $pool_id = $Class -replace ' ', ''
@@ -200,34 +231,39 @@ function Clone-ClassVMs {
     $last_index = -1
     foreach ($template in (Get-Templates -Class $Class)) {
         Write-Host "Cloning from template $($template.name);$($template.vmid)..." -BackgroundColor White -ForegroundColor Black
+
         foreach ($user in $users) {
             $node, $last_index = Get-NextNode $last_index
             #$name = "$($pool_id)-$($user)-$($template.name -replace '.*\d+','')"
+            Write-Host "Creating VM for $user with ID $vm_id on host $node"
 
-            Write-Host "Creating VM for $user with ID $vm_id on host $node with name $name"
-            
             $body = @{
                 newid = $vm_id
                 node = $node
                 vmid = $template.vmid
                 pool = $pool_id
+                full = 1
+                snapname = "Day1"
                 #name = $name
             }
-            #$vm = Invoke-PVEAPI -Route "nodes/$node/qemu/$($template.vmid)/clone" -Params @{Headers = $Vars.Headers; Method="POST"; Body = $body}
+            #$vm = Invoke-PVEAPI -Route "nodes/$node/qemu/$($template.vmid)/clone" -Params @{Headers = (Get-AccessTicket); Method="POST"; Body = $body}
+
+            if ($vm.Error) {
+                Write-Host "Retrying once..." -ForegroundColor White
+                #Invoke-PVEAPI -Route "nodes/$node/qemu/$($template.vmid)/clone" -Params @{Headers = (Get-AccessTicket); Method="POST"; Body = $body} -ErrorBehavior "Stop"
+            }
+            Set-ACL -Professor $professor -User $user -ID $vm_id
             $vm_id++
         }
     }
 }
-Get-AccessTicket -Credentials (Get-Credential -Credential $env:USERNAME)
 
-#write-host $Vars.Headers.Authorization
-#write-host $Vars.Headers.CSRFPreventionToken
-Clone-ClassVMs -Class "CIT 371-001" -CustomRosterPath "$HOME\Documents\custom.csv"
-#Invoke-PVEAPI -Route "pools/Templates" -Params @{Headers = $Vars.Headers; Method="GET"}
 
-#$vm = Invoke-PVEAPI -Route "nodes/COIVMHOST1/qemu/129/clone" -Params @{Headers = $Vars.Headers; Method="POST"; Body = @{newid=373;node="COIVMHOST1";vmid=129}}
-#if ($vm.Error) {
-#}
+#Get-AccessTicket
 
+#Clone-ClassVMs -Class "CIT 171-001" -CustomRosterPath "$HOME\Documents\custom.csv"
+$vm = Invoke-PVEAPI -Route "nodes/COIVMHOST1/qemu/129/clone" -Params @{Headers = (Get-AccessTicket); Method="POST"; Body = @{newid=374;node="COIVMHOST1";vmid=129;pool="CYS999-001";description=$markdown}}
+$vm
+#Set-ACL -Professor "poet2" -User "devorez1" -ID 374
 
 #Create-Pool -id "test"
